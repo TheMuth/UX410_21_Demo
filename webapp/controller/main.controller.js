311ef7a67fe2bd755684978f050a8036d170eab4
Leon Boeijen <trainer@themuth.nl>
2021-04-20 07:43:58 +0000

    First commit

---
+++ webapp/controller/main.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("studend00.sap.education.project1.controller.main", {
+			onInit: function () {
+
+			}
+		});
+	});
